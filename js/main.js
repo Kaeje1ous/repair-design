ecedd3d0b2a6f761fbde571e65afe76d50f72886
Kaejelous <52853794+Kaejelous@users.noreply.github.com>
2019-12-17 21:34:08 +0300

    feat: add new slider to projects layout

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-/*document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close')
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-    closeBtn.addEventListener('click', switchModal);
-
-
-    
-});*/
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle=modal]'),
-      closeBtn = $('.modal__close');
-
+      closeBtn = $('.modal__close'),
+      mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      }),
+      next = $('.swiper-button-next')
+      prev = $('.swiper-button-prev')
+      bullets = $('.swiper-pagination')
+      
   modalBtn.on('click', function () {
-    modal.toggleClass('modal--visible')
+    modal.toggleClass('modal--visible');
   });
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
-  })
+  });
+  next.css('left', prev.width() + 30 + bullets.width() + 30 );
+  bullets.css('left', prev.width() + 30  );
 });
+
+
 
