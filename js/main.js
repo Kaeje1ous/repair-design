88dd50aa497964bfc6922958f5525d7edfacc4ab
Kaejelous <52853794+Kaejelous@users.noreply.github.com>
2019-12-22 18:37:18 +0300

    feat: add validation

--- js/main.js
+++ js/main.js
@@ -27,6 +27,39 @@ $(document).ready(function () {
   bullets.css('left', prev.width() + 30  );
 
   new WOW().init();
+
+  // Валидация формы
+  $('.modal__form').validate({
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2
+      },
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Не менее 2 символов"
+      },
+      userPhone: "Телефон обязателен",
+      userEmail: {
+        required: "Email обязателен",
+        email: "Введите в формате: name@domain.com"
+      },
+    }
+
+  });
+  // маска для телефона
+  $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7 (___) __-__-___"});
+
 });
 
 
