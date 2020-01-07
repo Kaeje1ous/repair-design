e199d099b985f812d03b5f0e7a5bacc180eb11ac
Kaejelous <52853794+Kaejelous@users.noreply.github.com>
2020-01-07 20:55:14 +0300

    refact: refact validate errors

--- js/main.js
+++ js/main.js
@@ -88,11 +88,114 @@ $(document).ready(function () {
     }
   });
 
+  $('.control__form').validate({
+    errorClass: "invalid",
+    errorPlacement: function(error, element) {
+      if (element.attr("name") == "controlCheckbox") {
+  //Здесь пиши любые операции если чекбокс не отмечен
+          error.insertAfter(element.parent());
+      } else {
+          error.insertAfter(element);
+      }
+      return true;
+  },
+    ignore: ":disabled",
+    rules: {
+      controlCheckbox: {
+        required: true
+      },
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      userEmail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      controlCheckbox: {
+        required: "Необходимо пользовательское соглашение"
+      },
+      userName: {
+        required: "Заполните поле",
+        minlength: "Не менее 2 символов",
+        maxlength: "Не более 15 символов"
+      },
+      userPhone: "Заполните поле"
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log('Ajax сработал. Ответ сервера: ' + response);
+          alert('Форма отправлена, мы свяжемся с вами через 10 минут');
+          $(form)[0].reset();
+        }
+      });
+    }
+  });
+  $('.footer__form').validate({
+    errorClass: "invalid",
+    errorPlacement: function(error, element) {
+      if (element.attr("name") == "footerCheckbox") {
+  //Здесь пиши любые операции если чекбокс не отмечен
+          error.insertAfter(element.parent());
+      } else {
+          error.insertAfter(element);
+      }
+      return true;
+  },
+    rules: {
+      footerCheckbox: {
+        required: true
+      },
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      userMsg: {
+        required: true,
+        maxlength: 50
+      }
+    },
+    messages: {
+      footerCheckbox: {
+        required: "Необходимо пользовательское соглашение"
+      },
+      userName: {
+        required: "Заполните поле",
+        minlength: "Не менее 2 символов",
+        maxlength: "Не более 15 символов"
+      },
+      userPhone: "Заполните поле",
+      userMsg: {
+        required: "Заполните поле",
+        maxlength: "Не более 15 символов"
+      }
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        type: "POST",
+        url: "send.php",
+        data: $(form).serialize(),
+        success: function (response) {
+          console.log('Ajax сработал. Ответ сервера: ' + response);
+          alert('Форма отправлена, мы свяжемся с вами через 10 минут');
+          $(form)[0].reset();
+        }
+      });
+    }
+  });
   // маска для телефона
   $('[type=tel]').mask('+7(999) 999-99-99', {placeholder: "+7 (999) 999-99-99"});
-
   // yandex maps
-
   ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
             center: [47.244734, 39.723227],
@@ -100,12 +203,10 @@ $(document).ready(function () {
         }, {
             searchControlProvider: 'yandex#search'
         }),
-
         // Создаём макет содержимого.
         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
         ),
-
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
             hintContent: 'Собственный значок метки',
             balloonContent: 'Это красивая метка'
@@ -121,118 +222,13 @@ $(document).ready(function () {
             // её "ножки" (точки привязки).
             iconImageOffset: [-5, -38]
         });
-
-
+    myMap.behaviors.disable('scrollZoom');
+    myMap.behaviors.disable('drag');     
     myMap.geoObjects
         .add(myPlacemark);
 });
 });
 
-$('.control__form').validate({
-  errorClass: "invalid",
-  errorPlacement: function(error, element) {
-    if (element.attr("name") == "controlCheckbox") {
-//Здесь пиши любые операции если чекбокс не отмечен
-        error.insertAfter(element.parent());
-    } else {
-        error.insertAfter(element);
-    }
-    return true;
-},
-  ignore: ":disabled",
-  rules: {
-    controlCheckbox: {
-      required: true
-    },
-    userName: {
-      required: true,
-      minlength: 2,
-      maxlength: 15
-    },
-    userPhone: "required",
-    userEmail: {
-      required: true,
-      email: true
-    }
-  },
-  messages: {
-    controlCheckbox: {
-      required: "Необходимо пользовательское соглашение"
-    },
-    userName: {
-      required: "Заполните поле",
-      minlength: "Не менее 2 символов",
-      maxlength: "Не более 15 символов"
-    },
-    userPhone: "Заполните поле"
-  },
-  submitHandler: function(form) {
-    $.ajax({
-      type: "POST",
-      url: "send.php",
-      data: $(form).serialize(),
-      success: function (response) {
-        console.log('Ajax сработал. Ответ сервера: ' + response);
-        alert('Форма отправлена, мы свяжемся с вами через 10 минут');
-        $(form)[0].reset();
-      }
-    });
-  }
-});
 
-$('.footer__form').validate({
-  errorClass: "invalid",
-  errorPlacement: function(error, element) {
-    if (element.attr("name") == "footerCheckbox") {
-//Здесь пиши любые операции если чекбокс не отмечен
-        error.insertAfter(element.parent());
-    } else {
-        error.insertAfter(element);
-    }
-    return true;
-},
-  rules: {
-    footerCheckbox: {
-      required: true
-    },
-    userName: {
-      required: true,
-      minlength: 2,
-      maxlength: 15
-    },
-    userPhone: "required",
-    userMsg: {
-      required: true,
-      maxlength: 50
-    }
-  },
-  messages: {
-    footerCheckbox: {
-      required: "Необходимо пользовательское соглашение"
-    },
-    userName: {
-      required: "Заполните поле",
-      minlength: "Не менее 2 символов",
-      maxlength: "Не более 15 символов"
-    },
-    userPhone: "Заполните поле",
-    userMsg: {
-      required: "Заполните поле",
-      maxlength: "Не более 15 символов"
-    }
-  },
-  submitHandler: function(form) {
-    $.ajax({
-      type: "POST",
-      url: "send.php",
-      data: $(form).serialize(),
-      success: function (response) {
-        console.log('Ajax сработал. Ответ сервера: ' + response);
-        alert('Форма отправлена, мы свяжемся с вами через 10 минут');
-        $(form)[0].reset();
-      }
-    });
-  }
-});
 
 
